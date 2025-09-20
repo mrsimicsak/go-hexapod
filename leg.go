ee45678c9b66f674bd8074a6cc0506828e59ad31
mrsimicsak <mrsimicsak@gmail.com>
2025-09-19 23:23:48 -0700

    add ik

---
+++ leg.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"math"
+
+	"gobot.io/x/gobot/drivers/gpio"
+)
+
+const (
+	LENGTH_TROCHANTER = 50
+	LENGTH_FEMUR      = 80
+	LENGTH_TIBIA      = 120
+	PI                = 3.1415926535897932384626433832795
+	HALF_PI           = 1.5707963267948966192313216916398
+	TWO_PI            = 6.283185307179586476925286766559
+	DEG_TO_RAD        = 0.017453292519943295769236907684886
+	RAD_TO_DEG        = 57.295779513082320876798154814105
+	EULER             = 2.718281828459045235360287471352
+)
+
+var legZeroOffset = Vector3{130, 0, -120}
+
+type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type Leg struct {
+	ServoAngles    [3]float32
+	Servos         [3]*gpio.ServoDriver
+	TargetPosition Vector3
+}
+
+func calcLegServoAngles(pos Vector3, mountAngle float64, mirrored bool) (float64, float64, float64) {
+
+	// ###############################################################
+	// ### adjust coodinated for loacal space of leg (rotated leg) ###
+	// ###############################################################
+
+	var sinAlpha float64 = math.Sin(mountAngle * DEG_TO_RAD)
+	var cosAlpha float64 = math.Cos(mountAngle * DEG_TO_RAD)
+
+	var localCoordinates Vector3
+
+	localCoordinates.X = cosAlpha*pos.X + sinAlpha*pos.Y // adjust for leg mounting rotation and zer0 position
+	localCoordinates.X += legZeroOffset.X                // adjust for zero position (otherwise the leg origion is (0,0,0))
+
+	localCoordinates.Y = sinAlpha*pos.X - cosAlpha*pos.Y // we have to multiply by -1 to keep a right hand coordinat system
+	localCoordinates.Y += legZeroOffset.Y
+
+	localCoordinates.Z = pos.Z + legZeroOffset.Z
+
+	// ###################
+	// ### calc angles ###
+	// ###################
+
+	var noTrochanter float64 = math.Sqrt(math.Pow(localCoordinates.X, 2)+math.Pow(localCoordinates.Y, 2)) - LENGTH_TROCHANTER // distance between Servo 2 and the tip on the XY Plane
+	var servo2TipDistance float64 = math.Sqrt(math.Pow(noTrochanter, 2) + math.Pow(localCoordinates.Z, 2))                    // distance between servo 2 and the tip
+
+	var angle_0 float64 = math.Atan2(localCoordinates.Y, localCoordinates.X)*RAD_TO_DEG + 90
+
+	var angleRightSideTriangle float64 = math.Atan2(localCoordinates.Z, noTrochanter) * RAD_TO_DEG
+	var angleUnequalTriangle float64 = math.Acos((math.Pow(LENGTH_TIBIA, 2)-math.Pow(servo2TipDistance, 2)-math.Pow(LENGTH_FEMUR, 2))/(-2*servo2TipDistance*LENGTH_FEMUR)) * RAD_TO_DEG
+
+	var angleUnequalTriangle_2 float64 = math.Acos((math.Pow(servo2TipDistance, 2)-math.Pow(LENGTH_TIBIA, 2)-math.Pow(LENGTH_FEMUR, 2))/(-2*LENGTH_TIBIA*LENGTH_FEMUR)) * RAD_TO_DEG // second angle of unequal triangle
+
+	var angle_1 float64 = 0
+	var angle_2 float64 = 0
+
+	if mirrored { // if the leg is mirrored the servos are installed slightly different
+		angle_1 = (angleRightSideTriangle+angleUnequalTriangle)*-1 + 90
+		angle_2 = (angleUnequalTriangle_2)
+	} else {
+		angle_1 = (angleRightSideTriangle + angleUnequalTriangle) + 90
+		angle_2 = 180 - (angleUnequalTriangle_2)
+	}
+
+	// ####################################
+	// ### set target angles for servos ###
+	// ####################################
+
+	return angle_0, angle_1, angle_2
+}
--- main.go
+++ main.go
@@ -13,35 +13,37 @@ import (
 func main() {
 	r := raspi.NewAdaptor()
 	pca9685 := i2c.NewPCA9685Driver(r)
-	leg1_1 := gpio.NewServoDriver(pca9685, "0")
-    leg1_2 := gpio.NewServoDriver(pca9685, "1")
-    leg1_3 := gpio.NewServoDriver(pca9685, "2")
+
+	FrontLeft := Leg{Servos: [3]*gpio.ServoDriver{gpio.NewServoDriver(pca9685, "0"), gpio.NewServoDriver(pca9685, "1"), gpio.NewServoDriver(pca9685, "2")}}
 
 	work := func() {
 		pca9685.SetPWMFreq(60)
 
-		for i := 10; i < 150; i += 10 {
-			fmt.Println("Turning", i)
-			leg1_1.Move(uint8(i))
-            leg1_2.Move(uint8(i))
-            leg1_3.Move(uint8(i))
+		for i := 0; i < 10; i += 1 {
+			fmt.Println("Moving", i)
+			angle0, angle1, angle2 := calcLegServoAngles(Vector3{0, 0, float64(i)}, 0, false)
+			FrontLeft.Servos[0].Move(uint8(angle0))
+			FrontLeft.Servos[1].Move(uint8(angle1))
+			FrontLeft.Servos[2].Move(uint8(angle2))
 			time.Sleep(1 * time.Second)
 		}
 
-		for i := 150; i > 10; i -= 10 {
-			fmt.Println("Turning", i)
-            leg1_1.Move(uint8(i))
-            leg1_2.Move(uint8(i))
-            leg1_3.Move(uint8(i))
+		for i := 10; i > 0; i -= 1 {
+			fmt.Println("Moving", i)
+			angle0, angle1, angle2 := calcLegServoAngles(Vector3{0, 0, float64(i)}, 0, false)
+			FrontLeft.Servos[0].Move(uint8(angle0))
+			FrontLeft.Servos[1].Move(uint8(angle1))
+			FrontLeft.Servos[2].Move(uint8(angle2))
 			time.Sleep(1 * time.Second)
 		}
 	}
 
-	robot := gobot.NewRobot("servoBot",
+	robot := gobot.NewRobot("Hexapod",
 		[]gobot.Connection{r},
-		[]gobot.Device{pca9685, leg1_1, leg1_2, leg1_3},
+		[]gobot.Device{pca9685, FrontLeft.Servos[0], FrontLeft.Servos[1], FrontLeft.Servos[2]},
 		work,
 	)
 
 	robot.Start()
+
 }
